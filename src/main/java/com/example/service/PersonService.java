78e4626f583368f99f7a4a8b52d302b60864e6f5
Mohamedmedotito <83793850+Mohamedmedotito@users.noreply.github.com>
2022-11-02 15:57:09 +0200

    test
    test

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -40,12 +40,6 @@ public class PersonService {
 		p.setLastName("Mahmoud");
 		persons.add(p);
 		
-		p = new Person();
-		p.setId("5");
-		p.setAge(33);
-		p.setFirstName("gabry");
-		p.setLastName("Mahmoud");
-		persons.add(p);
 	}
 	public Person getPerson(String id) {
 		for(Person person:persons) {
